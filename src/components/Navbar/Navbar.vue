defec33a2f772652b371c9e1f691e1218ab0a8b8
ikeohachidi <ikeohachidi@gmail.com>
2022-02-13 18:41:44 +0100

    feat: create navbar component

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,22 +1,32 @@
 <template>
-	<nav>
-		<ul>
-			<li id="logo">
-				<img src="@/assets/logo.svg" alt="Logo">
-				petson.
-			</li>
+	<v-app-bar app color="primary" dark>
+		<div class="d-flex align-center">
+			<v-img
+				alt="Pet shop Logo"
+				class="shrink mr-2"
+				contain
+				src="@/assets/logo.svg"
+				transition="scale-transition"
+				width="30"
+			/>
+			<span>petson.</span>
+		</div>
 
-			<li class="ml-auto">Products</li>
-			<li>Promotions</li>
-			<li class="mr-auto">Blog</li>
-
-			<li class="action-btn">
-				<i class="ri-shopping-cart-2-fill"></i>
-				Cart
-			</li>
-			<li class="action-btn">Logout</li>
-		</ul>
-	</nav>
+		<v-spacer></v-spacer>
+		<div>
+			<v-btn plain>PRODUCTS</v-btn>
+			<v-btn plain>PROMOTIONS</v-btn>
+			<v-btn plain>BLOG</v-btn>
+		</div>
+		<v-spacer></v-spacer>
+	
+		<v-btn class="mr-2" outlined large @click="showCart">
+			<v-icon>mdi-cart</v-icon>
+			Cart
+			(0)
+		</v-btn>
+		<v-btn outlined large @click="showLogin">Login</v-btn>
+	</v-app-bar>
 </template>
 
 <script lang="ts">
@@ -24,38 +34,12 @@ import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class Navbar extends Vue {
+	private showCart() {
+		this.$emit('show-cart');
+	}
 
-}
-</script>
-
-<style lang="scss" scoped>
-#logo {
-	display: flex;
-	align-items: center;
-}
-
-nav {
-	display: flex;
-	padding: 10px 0;
-	background-color: var(--color-primary);
-	color: #fff;
-}
-
-ul {
-	width: 100%;
-	display: flex;
-	align-items: center;
-	margin: 0;
-	padding: 0;
-	list-style: none;
-
-	li {
-		padding: 10px 20px;
-		
-		&.action-btn {
-			border: 1px solid white;
-			border-radius: 6px;
-		}
+	private showLogin() {
+		this.$emit('show-login');
 	}
 }
-</style>
+</script>
