03c30683fdb6807e0fa05e61d978dd0ff4b16a23
ikeohachidi <ikeohachidi@gmail.com>
2022-02-14 21:52:03 +0100

    feat: add table options

--- src/views/Admin/Customers.vue
+++ src/views/Admin/Customers.vue
@@ -9,14 +9,57 @@
 			class="elevation-1"
 		>
 			<template #top>
-				<div class="d-flex justify-space-between pt-3 px-4">
-					<h3 class="text-h7">All customers</h3>
-					<div class="d-flex">
-						<v-btn color="primary" dense>
-							<v-icon>mdi-plus</v-icon>
-							add new customer
-						</v-btn>
-						
+				<div class="table-header d-flex justify-space-between align-center pl-5">
+					<h3 class="text-h7 mr-auto">All customers</h3>
+
+					<v-btn color="primary" depressed dense>
+						<v-icon>mdi-plus</v-icon>
+						add new customer
+					</v-btn>
+					<v-divider vertical class="ml-4"></v-divider>	
+					<div class="pa-5">
+						<p class="mb-0 pointer" @click="showTableOptions = !showTableOptions;">
+							Filter
+							<v-icon :class="{ 'rotate': showTableOptions }">mdi-chevron-down</v-icon>
+						</p>
+					</div>
+					<div class="table-header-options" v-if="showTableOptions">
+						<v-row>
+							<v-col cols="4">
+								<v-text-field label="Customer name" outlined dense>
+									<template #append>
+										<v-icon>mdi-magnify</v-icon>
+									</template>
+								</v-text-field>
+							</v-col>
+							<v-col cols="4">
+								<v-text-field label="Customer Email" outlined dense>
+									<template #append>
+										<v-icon>mdi-magnify</v-icon>
+									</template>
+								</v-text-field>
+							</v-col>
+							<v-col cols="4">
+								<v-text-field label="Customer Phone" outlined dense>
+									<template #append>
+										<v-icon>mdi-magnify</v-icon>
+									</template>
+								</v-text-field>
+							</v-col>
+							<v-col cols="4">
+								<v-text-field label="Customer Address" outlined dense>
+									<template #append>
+										<v-icon>mdi-magnify</v-icon>
+									</template>
+								</v-text-field>
+							</v-col>
+							<v-col cols="3">
+								<v-select label="Marketing Preference" outlined dense />
+							</v-col>
+							<v-col cols="1">
+								<v-btn depressed>reset</v-btn>
+							</v-col>
+						</v-row>
 					</div>
 				</div>
 			</template>
@@ -43,9 +86,29 @@ export default class Customers extends Vue {
 		{ text: 'Marketing Preferences', value: 'preference' }
 	]
 
+	private showTableOptions = false;
+
 	private customers = [];
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.table-header {
+	border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+	position: relative;
+
+	&-options {
+		padding: 20px;
+		background-color: #FAFAFA;
+		position: absolute;
+		top: 100%;
+		left: 0;
+		z-index: 10;
+		width: 100;
+	}
+}
+
+.rotate {
+	transform: rotateZ(180deg);
+}
 </style>
