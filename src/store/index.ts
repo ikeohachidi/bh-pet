623483bd1e61583180ec79c16db24018a92c6beb
ikeohachidi <ikeohachidi@gmail.com>
2022-02-15 23:57:48 +0100

    feat: add user store module

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,18 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import Admin, { State as AdminState } from './modules/admin';
+import User, { State as UserState } from './modules/admin';
 
 export type RootState = {
-	admin: AdminState
+	admin: AdminState,
+	user: UserState
 }
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
 	modules: {
-		admin: Admin
+		admin: Admin,
+		user: User
 	}
 })
---
+++ src/store/modules/user.ts
@@ -0,0 +1,156 @@
+import { ActionContext } from 'vuex';
+import { getStoreAccessors } from 'vuex-typescript';
+
+import http, { HTTPResponse } from '../http';
+import { RootState } from '..';
+import Credential from '@/models/Credential';
+
+export type State = {
+	users: unknown[];
+}
+
+type Context = ActionContext<State, RootState>;
+
+const state: State = {
+	users: []
+} 
+
+const store = {
+	state,
+	mutations: {
+	},
+	actions: {
+		getUser(context: Context, credential: Credential): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.get<HTTPResponse<void>>("/user")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		deleteUser(context: Context, credential: Credential): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.delete<HTTPResponse<void>>("/user")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		getUserOrders(context: Context): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.delete<HTTPResponse<void>>("/user/orders")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		editUser(context: Context): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.put<HTTPResponse<void>>("/user/edit")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		login(context: Context, credential: Credential): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.post<HTTPResponse<void>>("/user/login", credential)
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		logOut(context: Context): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.get<HTTPResponse<void>>("/user/logout")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		createUser(context: Context): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.post<HTTPResponse<void>>("/user/create")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		forgotPassword(context: Context): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.post<HTTPResponse<void>>("/user/forgot-password")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+		resetPasswordToken(context: Context): Promise<void> {
+			return new Promise((resolve, reject) => {
+				http.post<HTTPResponse<void>>("/user/reset-password-token")
+					.then(({ data }) => {
+						if (data.success) {
+							resolve();
+						} else {
+							reject(data.error);
+						}
+					})
+					.catch((error) => reject(error))
+			})
+		},
+	}
+}
+
+const { dispatch } = getStoreAccessors<State, RootState>("user");
+const { actions } = store;
+
+export const getUser = dispatch(actions.getUser);
+export const createUser = dispatch(actions.createUser);
+export const editUser = dispatch(actions.editUser);
+export const deleteUser = dispatch(actions.deleteUser);
+export const getUserOrders = dispatch(actions.getUserOrders);
+export const forgotPassword = dispatch(actions.forgotPassword);
+export const resetPasswordToken = dispatch(actions.resetPasswordToken);
+export const logOut = dispatch(actions.logOut);
+export const logIn = dispatch(actions.login);
+
+export default store;
