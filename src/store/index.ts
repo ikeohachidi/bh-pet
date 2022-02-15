696dd7890e895e6421d468e62f8eee20384359f1
ikeohachidi <ikeohachidi@gmail.com>
2022-02-16 00:21:25 +0100

    fix: user module import

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import Admin, { State as AdminState } from './modules/admin';
-import User, { State as UserState } from './modules/admin';
+import User, { State as UserState } from './modules/user';
 
 export type RootState = {
 	admin: AdminState,
