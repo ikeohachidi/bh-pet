53d72bae89cc434b7c9b8e9457164c86439ac3c1
ikeohachidi <ikeohachidi@gmail.com>
2022-02-15 09:26:57 +0100

    fix: remove unused import

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 
 import AuthForm from '@/components/AuthForm/AuthForm.vue';
 
