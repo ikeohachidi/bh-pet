0679bf86274b3812aefbed7f41e6654fce26660e
ikeohachidi <ikeohachidi@gmail.com>
2022-02-15 23:11:49 +0100

    feat: create HTTP response type

--- src/store/http.ts
+++ src/store/http.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
-const http = axios.create({
+export type HTTPResponse<T> = {
+	success: number;
+	data: T,
+	error: string,
+	errors: string[],
+	extra: string
+}
+
+export default axios.create({
 	baseURL: 'https://pet-shop.buckhill.com.hr/api/v1',
 })
-
-export default http;
