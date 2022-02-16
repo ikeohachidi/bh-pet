8d31cd5a4874d001a7d3e2fb39b58390b14c7cf8
ikeohachidi <ikeohachidi@gmail.com>
2022-02-16 16:21:38 +0100

    fix: add remaining user properties

--- src/types/User.ts
+++ src/types/User.ts
@@ -6,9 +6,13 @@ export default class User {
 	email_verified_at = '';
 	first_name = '';
 	last_name = '';
+	location = '';
 	phone_number = '';
+	password = '';
+	password_confirmation = '';
+	is_marketing = false;
 
-	get fullName() {
+	get fullName(): string {
 		return `${this.first_name} ${this.last_name}`;
 	}
 }
