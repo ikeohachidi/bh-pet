f9040b666761f4175fc0b080c241bac22f2d4bfd
ikeohachidi <ikeohachidi@gmail.com>
2022-02-16 20:21:43 +0100

    feat: add product card component

---
+++ src/components/ProductCard/ProductCard.vue
@@ -0,0 +1,50 @@
+<template>
+	<v-card class="pa-5" elevation="2">
+		<div class="product">
+			<div class="product-img-wrapper">
+				<img :src="`${api}/file/${product.metadata.image}`" :alt="product.title">
+			</div>
+			<h6 class="text-h6" :title="product.title">{{ product.title }}</h6>
+			<p class="text-capitalize">{{ product.brand.title }}</p>
+
+			<h6 class="text-h6">{{ product.price }} Kn</h6>
+		</div>
+	</v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import Product from '@/types/Product';
+
+@Component
+export default class ProductCard extends Vue {
+	@Prop({ default: () => ({})}) product!: Product;
+	
+	private api = process.env.VUE_APP_API;
+}
+</script>
+
+<style lang="scss" scoped>
+.product {
+	h6 {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+}
+
+.product-img-wrapper {
+	display: flex;
+	align-items: center;	
+	justify-content: center;
+	height: 150px;
+	width: 100%;
+
+	img {
+		width: 90%;
+		height: 100%;
+		object-fit: scale-down;
+		object-position: center;
+	}
+}
+</style>
