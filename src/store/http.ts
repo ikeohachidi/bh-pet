be442a80c7f038f8ecb8d9b354236985982b9869
ikeohachidi <ikeohachidi@gmail.com>
2022-02-16 14:34:28 +0100

    feat: add request interceptor to add token

--- src/store/http.ts
+++ src/store/http.ts
@@ -8,6 +8,18 @@ export type HTTPResponse<T> = {
 	extra: string
 }
 
-export default axios.create({
+const http = axios.create({
 	baseURL: 'https://pet-shop.buckhill.com.hr/api/v1',
 })
+
+http.interceptors.request.use((request) => {
+	const token = localStorage.getItem('pet-token');
+
+	if (request.headers && token) {
+		request.headers['Authorization'] = `Bearer ${token}`;
+	}
+
+	return request;
+})
+
+export default http;
