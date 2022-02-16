fc0edf8ff574664ac16f084aecea4e1c634b63d1
ikeohachidi <ikeohachidi@gmail.com>
2022-02-16 20:27:03 +0100

    feat: update types properties

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -1,3 +1,7 @@
 export default interface Category {
 	uuid: string;
+	title: string;
+	slug: string;
+	created_at: string;
+	updated_at: string;
 }
--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,8 +1,17 @@
+import Category from "./Category";
+
 export default interface Product {
 	uuid: string;
-	categoryUUID: string;
+	category_uuid: string;
 	title: string;
 	price: string;
 	description: string;
-	metadata: unknown;
+	metadata: {
+		brand: string;
+		image: string;
+	};
+	created_at: string;
+	updated_at: string;
+	deleted_at: string;
+	category: Category;
 }
