36dd5aaf68a614f5b44feb84873f7ab683df138c
ikeohachidi <ikeohachidi@gmail.com>
2022-02-17 17:30:00 +0100

    feat: make component update cart item

--- src/components/CartItem/CartItem.vue
+++ src/components/CartItem/CartItem.vue
@@ -1,16 +1,17 @@
 <template>
 	<section>
 		<div class="d-flex">
-			<div class="elevation-1 mr-4 rounded">
+			<div class="mr-4">
+				<img :src="`${api}/file/${product.metadata.image}`" :alt="product.title">
 			</div>
 
 			<div>
-				<h2 class="text-h6 font-weight-regular">Brit Care</h2>
-				<p>Animod</p>
-				<h1 class="my-6 text-h5">200 kn</h1>
+				<h2 class="text-h6 font-weight-regular">{{ product.title }}</h2>
+				<p>{{ product.brand.title }}</p>
+				<h1 class="my-6 text-h5">{{ product.price }} kn</h1>
 
 				<div class="d-flex">
-					<div class="d-flex" style="width: 140px">
+					<div class="d-flex mr-4" style="width: 140px">
 						<v-btn icon tile elevation="0">
 							<v-icon>mdi-minus</v-icon>
 						</v-btn>
@@ -19,9 +20,23 @@
 							<v-icon>mdi-plus</v-icon>
 						</v-btn>
 					</div>
-					<v-btn color="warning" outlined class="ml-5">
+					<v-btn 
+						color="error" 
+						elevation="0" 
+						@click="removeProductFromCart(product)"
+						v-if="isProductInCart"
+					>
 						<v-icon>mdi-cart-off</v-icon>
-						remove from cart
+						remove from cart	
+					</v-btn>
+					<v-btn 
+						color="primary" 
+						elevation="0" 
+						@click="addProductToCart(product)"
+						v-else
+					>
+						<v-icon>mdi-cart</v-icon>
+						add to cart
 					</v-btn>
 				</div>
 			</div>
@@ -32,11 +47,26 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
-import { Product } from '@/types/Cart';
+import Product from '@/types/Product';
+import { cartItems } from '@/store/modules/products';
 
 @Component
 export default class CartItem extends Vue {
-	@Prop({ default: () => ({}) }) item!: Product;
+	@Prop({ default: () => ({}) }) product!: Product;
+
+	private api = process.env.VUE_APP_API;
+
+	get cartItems() {
+		return cartItems(this.$store)
+	}
+
+	get itemInCart() {
+		return this.cartItems.find(item => item.productUUID === this.product.uuid);
+	}
+
+	get isProductInCart(): boolean {
+		return this.itemInCart !== undefined;
+	}
 }
 </script>
 
