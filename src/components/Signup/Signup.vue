7460c321c098ff1aedfb2e312a076a427192d7ae
ikeohachidi <ikeohachidi@gmail.com>
2022-02-20 14:02:13 +0100

    feat: add signup functionality to signup file

--- src/components/Signup/Signup.vue
+++ src/components/Signup/Signup.vue
@@ -5,17 +5,30 @@
 				<h2 class="text-center my-3">Signup</h2>
 				<v-row>
 					<v-col>
-						<v-text-field outlined label="Email Address *"></v-text-field>	
+						<v-text-field outlined label="First Name *" v-model="user.first_name"></v-text-field>	
 					</v-col>
 					<v-col>
-						<v-text-field outlined label="Last Name*"></v-text-field>	
+						<v-text-field outlined label="Last Name*" v-model="user.last_name"></v-text-field>	
 					</v-col>
 				</v-row>
-				<v-text-field outlined label="Email Address *"></v-text-field>	
-				<v-text-field outlined label="Password*" type="password"></v-text-field>	
-				<v-text-field outlined label="Confirm Password*" type="password"></v-text-field>	
+				<v-text-field outlined label="Email Address *" v-model="user.email"></v-text-field>	
+				<v-text-field outlined label="Password*" type="password" v-model="user.password"></v-text-field>	
+				<v-text-field outlined label="Confirm Password*" type="password" v-model="user.password_confirmation"></v-text-field>	
 
-				<v-btn class="w-100" width="100%" depressed color="primary">sign up</v-btn>
+				<v-checkbox 
+					label="I want to receive inspiration, marketing promotions and updates via email"
+					v-model="user.is_marketing"
+				></v-checkbox>
+
+				<v-btn 
+					class="w-100" 
+					width="100%" 
+					depressed 
+					color="primary" 
+					:disabled="isLoading"
+					:loading="isLoading"
+					@click="signUp"
+				>sign up</v-btn>
 
 				<template #footer>
 					<p class="link mx-auto mb-0" @click="showLogin">Already have an account? Login</p>
@@ -29,6 +42,8 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
 import AuthForm from '@/components/AuthForm/AuthForm.vue';
+import User from '@/types/User';
+import { createUser } from '@/store/modules/user';
 
 @Component({
 	components: {
@@ -38,12 +53,26 @@ import AuthForm from '@/components/AuthForm/AuthForm.vue';
 export default class Signup extends Vue {
 	@Prop({ default: false }) show!: boolean;
 
-	private close() {
+	private user: User = new User();
+	private isLoading = false;
+
+	private close(): void {
 		this.$emit('close');
 	}
 
-	private showLogin() {
+	private showLogin(): void {
 		this.$emit('show-login');
+	}
+
+	private signUp(): void {
+		this.isLoading = true;
+		createUser(this.$store, this.user)
+			.then(() => {
+				this.close();
+			})
+			.finally(() => {
+				this.isLoading = false;
+			})
 	}
 }
 </script>
