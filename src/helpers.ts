9e5005f12731c16ba36a0747041e47ae704e6c8e
ikeohachidi <ikeohachidi@gmail.com>
2022-02-22 22:28:20 +0100

    feat: add helper utilities

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,17 @@
 import dayjs from 'dayjs';
 
+export const getRandomNumber = (max: number, ignore: number[]): number => {
+	const rand = Math.floor(Math.random() * max);
+
+	if (ignore.includes(rand)) return getRandomNumber(max, ignore);
+
+	return rand;
+}
 
 export const formatTextToDate = (text: string): string => {
 	return dayjs(text).format('D.MM.YYYY')
+}
+
+export const getMonth = (text: string): number => {
+	return dayjs(text).month();
 }
